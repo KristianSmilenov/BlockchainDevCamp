7e75717ebb74cb93ef30fd3c0c57c9cb332d9716
Kristian Smilenov <kristian.smilenov@lykke.com>
2018-02-24 19:39:43 +0200

    Added binding to REST API for Transactions data

--- BlockExplorer/wwwroot/js/explorer.js
+++ BlockExplorer/wwwroot/js/explorer.js
@@ -19,21 +19,82 @@
         });
     }
 
+    function getBlocksList() {
+        var url = getNodeUrl() + '/api/blocks';
+        $.get(url, function (rawData) {
+            _.each(rawData, function (d) {
+                var timeAgoString = moment(d.dateCreated).fromNow();
+                d.dateCreated = timeAgoString;
+                var blockHashLabel = d.blockHash.substring(0, 25) + '...';
+                d.blockHashLabel = blockHashLabel;
+            });
+
+            var templateData = { target: rawData };
+            var template = _.template($("#blck-list-tmpl").text());
+            $("#blocksListPlaceHolder").html(template(templateData));
+        });
+    }
+
+    function getTransactions() {
+        var url = getNodeUrl() + '/api/transactions/pending';
+        $.get(url, function (rawData) {
+            _.each(rawData, function (d) {
+                var timeAgoString = moment(d.dateCreated).fromNow();
+                d.dateCreated = timeAgoString;
+                var fromLabel = d.from.substring(0, 10) + '...';
+                d.fromLabel = fromLabel;
+                var toLabel = d.to.substring(0, 10) + '...';
+                d.toLabel = toLabel;
+                var transactionHashLabel = d.transactionHashHex.substring(0, 10) + '...';
+                d.transactionHashLabel = transactionHashLabel;
+            });
+
+            var templateData = { target: rawData };
+            var template = _.template($("#trns-tmpl").text());
+            $("#transactionsPlaceHolder").html(template(templateData));
+        });
+    }
+
+    function getTransactionsList() {
+        var url = getNodeUrl() + '/api/transactions/pending';
+        $.get(url, function (rawData) {
+            _.each(rawData, function (d) {
+                var timeAgoString = moment(d.dateCreated).fromNow();
+                d.dateCreated = timeAgoString;
+                var fromLabel = d.from.substring(0, 10) + '...';
+                d.fromLabel = fromLabel;
+                var toLabel = d.to.substring(0, 10) + '...';
+                d.toLabel = toLabel;
+                var transactionHashLabel = d.transactionHashHex.substring(0, 10) + '...';
+                d.transactionHashLabel = transactionHashLabel;
+            });
+
+            var templateData = { target: rawData };
+            var template = _.template($("#trns-list-tmpl").text());
+            $("#transactionsListPlaceHolder").html(template(templateData));
+        });
+    }
+
     $('#buttonHome').click(function () {
         showView("homeSection");
         $(this).parent().addClass("active");
 
         getBlocks();
+        getTransactions();
     });
 
     $('#buttonBlocks').click(function () {
         showView("blocksSection");
         $(this).parent().addClass("active");
+
+        getBlocksList();
     });
 
     $('#buttonTransactions').click(function () {
         showView("transactionsSection");
         $(this).parent().addClass("active");
+
+        getTransactionsList();
     });
 
     $('#buttonAccounts').click(function () {
