502cca1555916468b975e22090a029b78b5d1554
Kristian Smilenov <kristian.smilenov@lykke.com>
2018-02-24 18:08:26 +0200

    Added binding to REST API for Blocks data

--- BlockExplorer/wwwroot/js/explorer.js
+++ BlockExplorer/wwwroot/js/explorer.js
@@ -1,22 +1,46 @@
 ﻿$(document).ready(function () {
     Date.prototype.toJSON = function () { return this.toISOString(); }
 
+    function getNodeUrl() {
+        return $("#nodeUrl").val();
+    }
+
+    function getBlocks() {
+        var url = getNodeUrl() + '/api/blocks';
+        $.get(url, function (rawData) {
+            _.each(rawData, function (d) {
+                var timeAgoString = moment(d.dateCreated).fromNow();
+                d.dateCreated = timeAgoString;
+            });
+
+            var templateData = { target: rawData };
+            var template = _.template($("#blck-tmpl").text());
+            $("#blocksPlaceHolder").html(template(templateData));
+        });
+    }
+
     $('#buttonHome').click(function () {
         showView("homeSection");
         $(this).parent().addClass("active");
+
+        getBlocks();
     });
+
     $('#buttonBlocks').click(function () {
         showView("blocksSection");
         $(this).parent().addClass("active");
     });
+
     $('#buttonTransactions').click(function () {
         showView("transactionsSection");
         $(this).parent().addClass("active");
     });
+
     $('#buttonAccounts').click(function () {
         showView("accountsSection");
         $(this).parent().addClass("active");
     });
+
     $('#buttonPeersMap').click(function () {
         showView("peersMapSection");
         $(this).parent().addClass("active");
@@ -32,5 +56,5 @@
         $('section').hide();
         $('#' + viewName).show();
     }
-    showView("homeSection");
+    $('#buttonHome').click();
 });
