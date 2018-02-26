3c688b3f642abb48fdd8700bf4204f8c77a80a48
Elian Kurtenkov <elian90@abv.bg>
2018-02-27 00:16:26 +0200

    ApPSettings files

---
+++ WebFaucet/AppSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebFaucet
+{
+    public class AppSettings
+    {
+        public string NodeUrl { get; set; }
+        public string PrivateKey { get; set; }
+        public string Address { get; set; }
+    }
+}
---
+++ WebWallet/AppSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebWallet
+{
+    public class AppSettings
+    {
+        public string NodeUrl{ get; set; }
+    }
+}
