663be8f0e4f4aa1d891f1d11718a6f28b1a198f9
Kristian Smilenov <kristian.smilenov@lykke.com>
2018-02-27 21:29:36 +0200

    Added validations and recaptcha to Faucet

--- WebFaucet/wwwroot/js/faucet.js
+++ WebFaucet/wwwroot/js/faucet.js
@@ -1,57 +1,91 @@
-﻿$(document).ready(function () {
+﻿var verifyCallback = function (data) {
+    if (data != "") {
+        $("#recaptchaError").text("");
+    }
+}
+
+var onloadCallback = function () {
+    grecaptcha.render('recaptcha', {
+        'sitekey': '6LeVZ0kUAAAAADk5pLtf6IAv72plFVAnvD-ZxcAB',
+        'callback': verifyCallback
+    });
+};
+
+$(document).ready(function () {
     Date.prototype.toJSON = function () { return this.toISOString(); }
 
-    $('#buttonBack').click(function () {
-        showView("sendCoins");
-    });
+    $("#blockchainNodeForm").validator();
+    $("#sendTransactionForm").validator();
+    $('#buttonSetNodeUrl').click(updateBlockchainNodeUrl);
+    $('#buttonSendCoins').click(sendCoins);
 
-    $('#buttonBack2').click(function () {
-        showView("sendCoins");
-    });
+    function updateBlockchainNodeUrl() {
+        var validator = $("#blockchainNodeForm").data("bs.validator");
+        validator.validate();
+        if (!validator.hasErrors()) {
+            $("#faucetConfiguration").toggle();
+        }
+    }
 
-    let curve = "secp256k1";
-    let sigalg = "SHA256withECDSA";
-    
-    $('#buttonSendCoins').click(buttonSendCoins);
+    function getNodeUrl() {
+        return $("#blockchainNodeUrl").val();
+    }
+
     $(document).on({
         ajaxStart: function () { $("#loadingBox").show() },
         ajaxStop: function () { $("#loadingBox").hide() }
     });
 
-    function buttonSendCoins() {
-        let recipient = $("#recipientAddress").val();
-        let nodeUrl = $("#blockchainNodeUrl").val();
-        let value = parseInt($("#requestedCoins").val());
+    function validate(event) {
+        event.preventDefault();
+        if (!document.getElementById('field').value) {
+            alert("You must add text to the required field");
+        } else {
+            grecaptcha.execute();
+        }
+    }
 
-        $.ajax({
-            url: 'api/transactions',
-            method: 'post',
-            dataType: 'json',
-            data: JSON.stringify({
-                nodeUrl: nodeUrl,
-                recipientAddress: recipient,
-                value: value
-            }),
-            contentType: "application/json",
-            success: function (data) {
-                if (!data.isValid || data.errorMessage || !data.transactionHash) {
-
-                    showView("coinsNotSent");
-                    $("#errorMessage").text(data.errorMessage);
-                }
-                else {
-                    showView("coinsSend");
-                    $("#numerOfCoins").text(value);
-                    $("#transactionHash").text(data.transactionHash);
-                }
-
-
-                $("#addressSent").text(recipient);
-            },
-            error: function (err) {
-                alert(JSON.parse(err));
+    function sendCoins() {
+        var validator = $("#sendTransactionForm").data("bs.validator");
+        validator.validate();
+        if (!validator.hasErrors()) {
+            $("#recaptchaError").text("");
+            var captcha = grecaptcha.getResponse();
+            if (!captcha) {
+                $("#recaptchaError").text("Please validate captcha");
+                return;
             }
-        });
+
+            let value = parseInt($("#transactionValue").val());
+            let recipient = $("#transactionRecipient").val();
+            let nodeUrl = getNodeUrl();
+            $.ajax({
+                url: 'api/transactions',
+                method: 'post',
+                dataType: 'json',
+                data: JSON.stringify({
+                    nodeUrl: nodeUrl,
+                    recipientAddress: recipient,
+                    value: value
+                }),
+                contentType: "application/json",
+                success: function (data) {
+                    if (!data.isValid || data.errorMessage || !data.transactionHash) {
+                        showView("coinsNotSent");
+                        $("#errorMessage").text(data.errorMessage);
+                    }
+                    else {
+                        showView("coinsSend");
+                        $("#numerOfCoins").text(value);
+                        $("#transactionHash").text(data.transactionHash);
+                    }
+                    $("#addressSent").text(recipient);
+                },
+                error: function (err) {
+                    alert(JSON.parse(err));
+                }
+            });
+        }
     }
 
     function showView(viewName) {
@@ -60,4 +94,12 @@
         $('#' + viewName).show();
     }
     showView("sendCoins");
+
+    $('#buttonBack').click(function () {
+        showView("sendCoins");
+    });
+
+    $('#buttonBackError').click(function () {
+        showView("sendCoins");
+    });
 });
